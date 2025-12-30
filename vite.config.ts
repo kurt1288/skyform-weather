b09ee5b4b6163bde8a3dd75d89720f8706ea014f
Kurt <3249028+kurt1288@users.noreply.github.com>
2025-12-30 11:38:35 -0800

    use vite plugin for manifest creation

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,35 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Skyform',
+        short_name: 'Skyform',
+        description: 'Skyform Weather App',
+        theme_color: '#161616',
+        background_color: '#161616',
+        display: 'standalone',
+        start_url: './index.html',
+        icons: [
+          {
+            src: 'icons/icon-384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-1024.png',
+            sizes: '1024x1024',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
